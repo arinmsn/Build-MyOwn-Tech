4ef4d0acbdad10873710b63c5919fcb8de22c817
arinmsn <arinmsn@gmail.com>
2020-06-03 12:41:12 -0700

    The ball randomly moves across the screen. Color is randomized.

--- P5_Challenges/6_Mitosis/P5/cell.js
+++ P5_Challenges/6_Mitosis/P5/cell.js
@@ -1,11 +1,18 @@
 function Cell() {
   this.pos = createVector(random(width), random(height));
   // radius
-  this.r = 20;
+  this.r = 80;
+  // random(x, y) is range of colors for red & blue
+  this.c = color(random(100, 255), 0, random(100, 255));
 
-  this.move = function () {};
+  this.move = function () {
+    var velocity = p5.Vector.random2D();
+    this.pos.add(velocity);
+  };
 
   this.show = function () {
+    noStroke();
+    fill(this.c);
     ellipse(this.pos.x, this.pos.y, this.r, this.r);
   };
 }
