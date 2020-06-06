7e29586b6174a77203500cab3673857a661a9cd5
arinmsn <arinmsn@gmail.com>
2020-06-05 17:55:25 -0700

    Sun's children now show up and rotate around Sun.

--- P5_Challenges/7_SolarSystemGenerator/P5/planet.js
+++ P5_Challenges/7_SolarSystemGenerator/P5/planet.js
@@ -1,12 +1,42 @@
 class Planet {
-  constructor(radius, distance) {
+  constructor(radius, distance, orbitSpeed, angle) {
     this.radius = radius;
     this.distance = distance;
+    this.angle = angle;
+    this.orbitSpeed = orbitSpeed;
+    this.planets = [];
+  }
+
+  orbit() {
+    this.angle += this.orbitSpeed;
+    for (let i in this.planets) {
+      this.planets[i].orbit();
+    }
+  }
+
+  spawnMoons(total, level) {
+    for (let i = 0; i < total; i++) {
+      let r = this.radius / (level * 2);
+      let d = random(50, 150);
+      let o = random(-0.1, 0.1);
+      let a = random(TWO_PI);
+      this.planets.push(new Planet(r, d / level, o, a));
+      if (level < 1) {
+        let num = Math.floor(random(0, 4));
+        this.planets[i].spawnMoons(num, level + 1);
+      }
+    }
   }
 
   show() {
+    push();
     fill(255);
+    rotate(this.angle);
     translate(this.distance, 0);
-    ellipse(0, 0, this.radius * 2, this.radius * 2);
+    ellipse(0, 0, this.radius * 2);
+    for (let i in this.planets) {
+      this.planets[i].show();
+    }
+    pop();
   }
 }
