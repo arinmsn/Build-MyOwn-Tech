8c79ae2902fd6043c91b49848922a2b1698d3aec
arinmsn <arinmsn@gmail.com>
2020-06-20 17:32:27 -0700

    Current cell is shown in a different color so we can just follow along using this.highlight func.

--- P5_Challenges/8_MazeGenerator/P5/sketch.js
+++ P5_Challenges/8_MazeGenerator/P5/sketch.js
@@ -33,6 +33,7 @@ function draw() {
   var next = current.checkNeighbors();
   if (next) {
     next.visited = true;
+    current.highlight();
     // Step 3: Remove wall between current cell and chosen cell
     removeWalls(current, next);
 
@@ -90,6 +91,14 @@ function Cell(i, j) {
     } else {
       return undefined;
     }
+  };
+
+  this.highlight = function () {
+    var x = this.i * w;
+    var y = this.j * w;
+    noStroke();
+    fill(0, 0, 255, 100);
+    rect(x, y, w, w);
   };
 
   this.show = function () {
