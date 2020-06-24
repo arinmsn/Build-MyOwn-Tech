d7848ff258a118077996c082cb329c822929fe58
arinmsn <arinmsn@gmail.com>
2020-06-24 14:06:48 -0700

    The Lorenz System is being generated but it's only in color red.

--- P5_Challenges/10_LorenzAttractor/P5/sketch.js
+++ P5_Challenges/10_LorenzAttractor/P5/sketch.js
@@ -25,4 +25,38 @@ function draw() {
   x = x + dx;
   y = y + dy;
   z = z + dz;
+
+  points.push(new p5.Vector(x, y, z));
+
+  translate(0, 0, -80);
+  let camX = map(mouseX, 0, width, -200, 200);
+  let camY = map(mouseY, 0, height, -200, 200);
+  camera(
+    camX,
+    camY,
+    // prettier-ignore
+    height / 2.0 / tan((PI * 30.0) / 180.0),
+    0,
+    0,
+    0,
+    0,
+    1,
+    0
+  );
+  scale(5);
+  stroke(255);
+  noFill();
+
+  beginShape();
+  for (let v of points) {
+    stroke(hue, 255, 255);
+    vertex(v.x, v.y, v.z);
+
+    hue += 1;
+    if (hue > 255) {
+      hue = 0;
+    }
+  }
+
+  endShape();
 }
