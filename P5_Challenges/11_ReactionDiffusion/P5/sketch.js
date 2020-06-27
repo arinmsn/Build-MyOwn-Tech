7e54d59ef4639c1c0326b046d623c93dd4456250
arinmsn <arinmsn@gmail.com>
2020-06-26 18:27:08 -0700

    Basic setup of the grid (A and B Chemicals)

--- P5_Challenges/11_ReactionDiffusion/P5/sketch.js
+++ P5_Challenges/11_ReactionDiffusion/P5/sketch.js
@@ -1,5 +1,18 @@
+var grid;
+
 function setup() {
   createCanvas(200, 200);
+  pixelDensity(1);
+  grid = [];
+  for (let x = 0; x < innerWidth; x++) {
+    grid[x] = [];
+    for (let y = 0; y < innerHeight; y++) {
+      grid[x][y] = {
+        a: 0,
+        b: 0,
+      };
+    }
+  }
 }
 
 function draw() {
