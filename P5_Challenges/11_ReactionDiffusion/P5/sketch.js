92b3b78007ce52afdd0a1234a649da61a0415917
arinmsn <arinmsn@gmail.com>
2020-06-26 18:41:17 -0700

    Fixed some typos that were due to auto-correction. Now we have a way of generating pixel color  based on the amount of chemicals.

--- P5_Challenges/11_ReactionDiffusion/P5/sketch.js
+++ P5_Challenges/11_ReactionDiffusion/P5/sketch.js
@@ -2,17 +2,17 @@ let grid;
 let next;
 
 function setup() {
-  createCanvas(200, 200);
+  createCanvas(500, 500);
   pixelDensity(1);
   grid = [];
   next = [];
-  for (let x = 0; x < innerWidth; x++) {
+  for (let x = 0; x < width; x++) {
     grid[x] = [];
     next[x] = [];
-    for (let y = 0; y < innerHeight; y++) {
+    for (let y = 0; y < height; y++) {
       grid[x][y] = {
-        a: 0,
-        b: 0,
+        a: random(1),
+        b: random(1),
       };
       next[x][y] = {
         a: 0,
@@ -25,13 +25,13 @@ function setup() {
 function draw() {
   background(51);
   loadPixels();
-  for (let x = 0; x < innerWidth; x++) {
-    for (let y = 0; y < innerHeight; y++) {
+  for (let x = 0; x < width; x++) {
+    for (let y = 0; y < height; y++) {
       var c = color(255, 0, 100);
       var pix = (x + y * width) * 4;
-      pixels[pix + 0] = grid[x][y].a;
+      pixels[pix + 0] = floor(grid[x][y].a * 255);
       pixels[pix + 1] = 0;
-      pixels[pix + 2] = grid[x][y].b;
+      pixels[pix + 2] = floor(grid[x][y].b * 255);
       pixels[pix + 3] = 255;
     }
   }
