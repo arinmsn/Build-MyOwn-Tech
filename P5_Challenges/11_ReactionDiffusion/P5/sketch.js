bdb6e9f6d32a07674bf705cba894a9cf6fec1b98
arinmsn <arinmsn@gmail.com>
2020-06-26 18:37:12 -0700

    We are basing the colors of pixels based on the grid vs hardcoding it.

--- P5_Challenges/11_ReactionDiffusion/P5/sketch.js
+++ P5_Challenges/11_ReactionDiffusion/P5/sketch.js
@@ -1,13 +1,20 @@
-var grid;
+let grid;
+let next;
 
 function setup() {
   createCanvas(200, 200);
   pixelDensity(1);
   grid = [];
+  next = [];
   for (let x = 0; x < innerWidth; x++) {
     grid[x] = [];
+    next[x] = [];
     for (let y = 0; y < innerHeight; y++) {
       grid[x][y] = {
+        a: 0,
+        b: 0,
+      };
+      next[x][y] = {
         a: 0,
         b: 0,
       };
@@ -17,4 +24,16 @@ function setup() {
 
 function draw() {
   background(51);
+  loadPixels();
+  for (let x = 0; x < innerWidth; x++) {
+    for (let y = 0; y < innerHeight; y++) {
+      var c = color(255, 0, 100);
+      var pix = (x + y * width) * 4;
+      pixels[pix + 0] = grid[x][y].a;
+      pixels[pix + 1] = 0;
+      pixels[pix + 2] = grid[x][y].b;
+      pixels[pix + 3] = 255;
+    }
+  }
+  updatePixels();
 }
