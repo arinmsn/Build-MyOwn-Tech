c46a8067ee9fb9caef00829e08c9b86ad8d45f29
arinmsn <arinmsn@gmail.com>
2020-06-29 17:23:05 -0700

    There was a typo in our equatiosn for chemical A and B ( + instead of *). A small portion of chemicals (blue) is shown (poured) into a red background.

--- P5_Challenges/11_ReactionDiffusion/P5/sketch.js
+++ P5_Challenges/11_ReactionDiffusion/P5/sketch.js
@@ -18,8 +18,8 @@ function setup() {
     next[x] = [];
     for (let y = 0; y < height; y++) {
       grid[x][y] = {
-        a: random(1),
-        b: random(1),
+        a: 1,
+        b: 0,
       };
       next[x][y] = {
         a: 0,
@@ -27,17 +27,23 @@ function setup() {
       };
     }
   }
+
+  for (var i = 100; i < 110; i++) {
+    for (var j = 100; j < 110; j++) {
+      grid[i][j].b = 1;
+    }
+  }
 }
 
 function draw() {
   background(51);
 
-  for (let x = 0; x < width; x++) {
-    for (let y = 0; y < height; y++) {
+  for (let x = 1; x < width - 1; x++) {
+    for (let y = 1; y < height - 1; y++) {
       let a = grid[x][y].a;
       let b = grid[x][y].b;
-      next[x][y].a = a + (dA + laplaceA() * a) - a * b * b + feed * (1 - a);
-      next[x][y].b = b + (dB + laplaceB() * b) + a * b * b - (k + feed) * b;
+      next[x][y].a = a + dA * laplaceA(x, y) - a * b * b + feed * (1 - a);
+      next[x][y].b = b + dB * laplaceB(x, y) + a * b * b - (k + feed) * b;
     }
   }
 
@@ -55,6 +61,44 @@ function draw() {
   updatePixels();
 
   swap();
+}
+
+function laplaceA(x, y) {
+  let sumA = 0;
+  // sumA += center a given grid * weight
+  sumA += grid[x][y].a * -1;
+  // cell adjacent to left
+  sumA += grid[x - 1][y].a * 0.2;
+  // cell adjacent to right
+  sumA += grid[x + 1][y].a * 0.2;
+  // top and bottom cells
+  sumA += grid[x][y + 1].a * 0.2;
+  sumA += grid[x][y - 1].a * 0.2;
+  // upper left cell
+  sumA += grid[x - 1][y - 1].a * 0.05;
+  sumA += grid[x + 1][y - 1].a * 0.05;
+  sumA += grid[x + 1][y + 1].a * 0.05;
+  sumA += grid[x - 1][y + 1].a * 0.05;
+  return sumA;
+}
+
+function laplaceB(x, y) {
+  let sumB = 0;
+  // sumA += center a given grid * weight
+  sumB += grid[x][y].b * -1;
+  // cell adjacent to left
+  sumB += grid[x - 1][y].b * 0.2;
+  // cell adjacent to right
+  sumB += grid[x + 1][y].b * 0.2;
+  // top and bottom cells
+  sumB += grid[x][y + 1].b * 0.2;
+  sumB += grid[x][y - 1].b * 0.2;
+  // upper left cell
+  sumB += grid[x - 1][y - 1].b * 0.05;
+  sumB += grid[x + 1][y - 1].b * 0.05;
+  sumB += grid[x + 1][y + 1].b * 0.05;
+  sumB += grid[x - 1][y + 1].b * 0.05;
+  return sumB;
 }
 
 function swap() {
