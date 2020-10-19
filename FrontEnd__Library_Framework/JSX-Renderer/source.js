0cfca21a5f27a1f2c27deefa57d1d5e57ce6d595
arinmsn <arinmsn@gmail.com>
2020-10-19 09:01:33 -0700

    Did not use Babel to transpile. Done with main code

--- FrontEnd__Library_Framework/JSX-Renderer/source.js
+++ FrontEnd__Library_Framework/JSX-Renderer/source.js
@@ -1,8 +1,34 @@
-function h(nodeName, attributes, ...args) {
-  let children = args.length ? [].concat(...args) : null;
-  return { nodeName, attributes, children };
+let items = ["apple", "pineapple", "orange", "grapes"];
+
+function foo(items) {
+  return items.map((p) => <li>{p}</li>);
 }
 
+let list = render(<ul>{items.map(item)}</ul>);
+
+document.body.appendChild(list);
+
+// JSX to VDOM
+let vdom = (
+  <div id="foo">
+    <p>A imple JSX DOX renderer!</p>
+    <ul>{foo(ITEMS)}</ul>
+  </div>
+);
+
+// VDOM to DOM
+let dom = render(vdom);
+
+// Add the tree to <body>
+document.body.appendChild(dom);
+
+// Virtual DOM is just a JSON - each "VNode" is an object w/ 3 properties
+let json = JSON.stringify(vdom, null, "  ");
+
+// JSX -> DOM -> DOM in one step
+document.body.appendChild(render(<pre>{json}</pre>));
+
+// Render virtual DOM to real DOM
 function render(vnode) {
   // Strings just convert to #text Nodes
   if (vnode.split) {
@@ -20,4 +46,9 @@ function render(vnode) {
   (vnode.children || []).forEach((c) => n.appendChild(render(c)));
 
   return n;
+}
+
+function h(nodeName, attributes, ...args) {
+  let children = args.length ? [].concat(...args) : null;
+  return { nodeName, attributes, children };
 }
