5d2306686cadb68438d47e155751ad3afccc336e
Arin <arinmsn@gmail.com>
2021-03-03 05:09:32 -0800

    Will add testing later. Using some dummy data to test app.

--- TS_todo/src/index.ts
+++ TS_todo/src/index.ts
@@ -1,2 +1,19 @@
+import { TodoItem } from "./todoItem";
+import { TodoCollection } from "./todoCollection";
+
+let todos: TodoItem[] = [
+    new TodoItem(1, "Buy printer copy paper"), new TodoItem(2, "Buy toothpaste"),
+    new TodoItem(3, "Buy Hockey season tickets"), new TodoItem(4, "Make dentist appointment", true)
+];
+
+let collection: TodoCollection = new TodoCollection("Edmond", todos);
+
 console.clear();
-console.log("Arin's TypeScript Todo List")
+console.log(`${collection.userName}'s Todo List: `);
+
+let newId: number = collection.addTodo("Go cycling");
+let todoItem: TodoItem = collection.getTodoById(newId);
+console.log(JSON.stringify(todoItem));
+todoItem.printDetails();
+
+// collection.addTodo(todoItem);
