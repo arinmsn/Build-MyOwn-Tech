259f2db3181df33b06d301928122d8c3771c6329
Arin <arinmsn@gmail.com>
2021-03-29 08:39:37 -0700

    App works (sorts 3 ints and ',' is +ed between ints. Next step is to shorten code.

--- Books/ProgrammingPrinciplesPractice_inCPP/Ch3/Exercises/No6_SortNums_AddComma.cpp
+++ Books/ProgrammingPrinciplesPractice_inCPP/Ch3/Exercises/No6_SortNums_AddComma.cpp
@@ -1,22 +1,33 @@
+/*
+Write a program that prompts the user to enter three integer values, and
+then outputs the values in numerical sequence separated by commas. So,
+if the user enters the values 10 4 6 , the output should be 4, 6, 10 . If two
+values are the same, they should just be ordered together. So, the input
+4 5 4 should give 4, 4, 5 .
+*/
+
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <algorithm> // for sort()
 using namespace std;
 
 int main()
 {
     int user_ints;
-    vector<int> userInputs {};
+    vector<int> v {};
+    int num1, num2, num3;
     cout << "\nPlease, enter 3 integers: ";
     
-    while (cin >> user_ints) 
-    {
-        userInputs.push_back(user_ints);
-    }
-    cout << user_ints << endl;
-    // for (auto x : userInputs)
+    cin >> num1 >> num2 >> num3;
+    
+    v.push_back(num1);
+    v.push_back(num2);
+    v.push_back(num3);
+    
+    sort(v.begin(), v.end());
+    // for (auto i: v)
     // {
-    //     cout << x << ", ";
+    //     cout << i << ", ";
     // }
-
+    cout << v[0] << ", " << v[1] << ", " << v[2] << endl;
 }
