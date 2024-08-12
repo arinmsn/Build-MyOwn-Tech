fed6eb3577a4d53d916fc8ff600e8895638da154
Arin Minasian <arinmsn@gmail.com>
2024-08-12 12:44:17 -0400

    Day 7 - Hangman - initial commit / basic layout

---
+++ 100 Days of Python/hangman.py
@@ -0,0 +1,8 @@
+# Day 7 - Hangman
+
+# TODO - create a list of random words (at least 50)
+# TODO - Randomly choose a word out of the list above
+# TODO - Based on length of the word, show _ underscores for each letter in the word
+# TODO - Ask user to guess a letter and assign their response to a variable. Make guess lowercase.
+# TODO-3 - Check if the letter the user guessed  is one of the letters in the chosen_word. Print "Right" if it
+#  is, "Wrong" if it's not.
