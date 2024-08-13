36e671ef172975f3966c089f5a319c8b59fd3be7
Arin Minasian <arinmsn@gmail.com>
2024-08-12 22:23:02 -0400

    Word generation out of an English dictionary for hangman is done.

--- 100 Days of Python/hangman.py
+++ 100 Days of Python/hangman.py
@@ -1,7 +1,35 @@
 # Day 7 - Hangman
+import json
+import random
+import re
+"""
+The dictionary.json was used from WebstersEnglishDictionary Github project.
+
+https://github.com/matthewreagan/WebstersEnglishDictionary?tab=readme-ov-file
+Matt Reagan - Website: http://sound-of-silence.com/
+Webster's Unabridged English Dictionary provided by Project Gutenberg
+
+"""
 
 # TODO - create a list of random words (at least 50)
 # TODO - Randomly choose a word out of the list above
+random_word = None
+filename = './data/dictionary.json'
+
+with open(filename, 'r') as file:
+    words_dict = json.load(file)
+
+# Regular expression pattern to match only single words containing alphabetic characters
+pattern = re.compile(r'^[A-Za-z]+$')
+
+# Filter the dictionary keys to ensure they contain only single words (no space)
+# (e.g. "norfolk spaniel")
+valid_words = [key for key in words_dict.keys() if pattern.match(key)]
+
+if valid_words:
+    random_word = random.choice(valid_words)
+
+print(random_word)
 # TODO - Based on length of the word, show _ underscores for each letter in the word
 # TODO - Ask user to guess a letter and assign their response to a variable. Make guess lowercase.
 # TODO-3 - Check if the letter the user guessed  is one of the letters in the chosen_word. Print "Right" if it
