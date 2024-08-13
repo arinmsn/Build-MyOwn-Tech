b105dd50955844be0673b9bce3f10eaf0af214b0
Arin Minasian <arinmsn@gmail.com>
2024-08-12 21:08:07 -0400

    169. Majority Element

---
+++ LeetCode/majority_element_169.py
@@ -0,0 +1,50 @@
+"""
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times.
+You may assume that the majority element always exists in the array.
+
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+
+
+
+Constraints:
+
+    n == nums.length
+    1 <= n <= 5 * 104
+    -109 <= nums[i] <= 109
+
+
+
+"""
+
+
+# Boyer-Moore Voting Algorithm
+
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+
+        return candidate
+
+# Time complexity: O(n) - we only iterate through the whole input list of length n once.
+# Space complexity: O(1) - we only use a constant amount of extra space regardless of the size of the input list.
