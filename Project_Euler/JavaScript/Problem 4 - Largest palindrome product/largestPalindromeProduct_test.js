eaa00182d42df420763f84d91717cfc93fd4c458
Arin Minasian <arinmsn@gmail.com>
2024-08-18 23:42:17 -0400

    Problem 4 - Tests

---
+++ Project_Euler/JavaScript/Problem 4 - Largest palindrome product/largestPalindromeProduct_test.js
@@ -0,0 +1,37 @@
+const assert = require('assert');
+const { isPalindromeNumber, largestPalindromeProduct } = require('./largestPalindromeProduct');
+
+// Test cases
+describe('Largest Palindrom Tests', function () {
+	it('should correctly identify a palindrom number', function () {
+		assert.strictEqual(isPalindromeNumber(121), true);
+		assert.strictEqual(isPalindromeNumber(131), true);
+		assert.strictEqual(isPalindromeNumber(242), true);
+		assert.strictEqual(isPalindromeNumber(3553), true);
+		assert.strictEqual(isPalindromeNumber(4664), true);
+		assert.strictEqual(isPalindromeNumber(5775), true);
+		assert.strictEqual(isPalindromeNumber(6886), true);
+		assert.strictEqual(isPalindromeNumber(7997), true);
+		assert.strictEqual(isPalindromeNumber(9009), true);
+		assert.strictEqual(isPalindromeNumber(12321), true);
+		assert.strictEqual(isPalindromeNumber(45654), true);
+		assert.strictEqual(isPalindromeNumber(123), false);
+		assert.strictEqual(isPalindromeNumber(234), false);
+		assert.strictEqual(isPalindromeNumber(3456), false);
+		assert.strictEqual(isPalindromeNumber(5678), false);
+		assert.strictEqual(isPalindromeNumber(6789), false);
+		assert.strictEqual(isPalindromeNumber(7890), false);
+		assert.strictEqual(isPalindromeNumber(8901), false);
+		assert.strictEqual(isPalindromeNumber(9012), false);
+		assert.strictEqual(isPalindromeNumber(12345), false);
+		assert.strictEqual(isPalindromeNumber(45678), false);
+	});
+
+	it('should find the largest palindrome product for 2-digit numbers', function () {
+		assert.strictEqual(largestPalindromeProduct(2), 9009);
+	});
+
+	it('should find the largest palindrome product for 3-digit numbers', function () {
+		assert.strictEqual(largestPalindromeProduct(3), 906609);
+	});
+});
