0114d616574e180b89051107eeaf522a70447c56
Arin Minasian <arinmsn@gmail.com>
2024-08-19 21:59:54 -0400

    2015 - Day 1: Part II

--- Advent of Code/2015/Day 1/not_quite_lisp.py
+++ Advent of Code/2015/Day 1/not_quite_lisp.py
@@ -6,14 +6,18 @@ with open("data.txt", "r") as file:
 
 def which_floor_to_go(str):
     floor = 0
+    pos = 0
     for char in str:
         if char == "(":
             floor += 1
         elif char == ")":
             floor -= 1
-    return floor
+        pos +=1
+        if floor == -1:
+            return pos
+    #return floor
 
 
-print(which_floor_to_go(str)) # Part I: 280
+print(which_floor_to_go(str)) # Part I: floor 280; Part II: position 1797
 
 
