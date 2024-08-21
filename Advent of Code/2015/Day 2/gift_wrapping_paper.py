4ec75773cd34402c7b5d6c21bb38dd909dca64df
Arin Minasian <arinmsn@gmail.com>
2024-08-21 00:04:28 -0400

    2015 - Day 2: Part II

--- Advent of Code/2015/Day 2/gift_wrapping_paper.py
+++ Advent of Code/2015/Day 2/gift_wrapping_paper.py
@@ -1,4 +1,4 @@
-def calculate_gift_wrapping_paper(length, width, height):
+def find_smallest_two_numbers(length, width, height):
     smallest_side = min(length, width, height)
 
     if smallest_side == length:
@@ -8,6 +8,11 @@ def calculate_gift_wrapping_paper(length, width, height):
     else:
         second_smallest = min(length, width)
 
+    return smallest_side, second_smallest
+def calculate_gift_wrapping_paper(length, width, height):
+
+    smallest_side, second_smallest = find_smallest_two_numbers(length, width, height)
+
     extra_slack = smallest_side * second_smallest
 
     surface_area = (2*length*width) + (2*width*height) + (2*height*length)
@@ -16,8 +21,19 @@ def calculate_gift_wrapping_paper(length, width, height):
     return final_wrapping_paper_size
 
 
+def calculate_ribbon_length_to_order(length, width, height):
+    a, b = find_smallest_two_numbers(length, width, height)
+    size_of_ribbon = (a + a + b + b)
+    size_of_ribbon_bow = length * width * height
+    total_size_of_ribbon = size_of_ribbon + size_of_ribbon_bow
+
+    return total_size_of_ribbon
+
+
 with open('data.txt', 'r') as file:
-    total = 0
+    total_wrapping_paper = 0
+    total_ribbion_length = 0
+
     for line in file:
         # Remove trailing newline or spaces
         line = line.strip()
@@ -28,9 +44,11 @@ with open('data.txt', 'r') as file:
         width = int(width)
         height = int(height)
 
-        total += calculate_gift_wrapping_paper(length, width, height)
+        total_wrapping_paper += calculate_gift_wrapping_paper(length, width, height)
+        total_ribbion_length+= calculate_ribbon_length_to_order(length, width, height)
 
-    print(f"Total gift wrapping paper: {total}") # Part I: 1606483
+    print(f"Total gift wrapping paper: {total_wrapping_paper}") # Part I: 1606483
+    print(f"Total ribbon length: {total_ribbion_length}") # Part II: 3842356
 
 
 
