64e7fb06dee0ddf68fed969b66d12e30772288f7
Arin Minasian <arinmsn@gmail.com>
2024-08-21 00:04:39 -0400

    2015 - Day 2: Part II - More tests

--- Advent of Code/2015/Day 2/test_gift_wrapping_paper.py
+++ Advent of Code/2015/Day 2/test_gift_wrapping_paper.py
@@ -1,8 +1,21 @@
 import pytest
 
-from gift_wrapping_paper import calculate_gift_wrapping_paper
+from gift_wrapping_paper import find_smallest_two_numbers, calculate_gift_wrapping_paper
+from gift_wrapping_paper import calculate_ribbon_length_to_order
+
+
+def test_find_smallest_two_numbers():
+    assert find_smallest_two_numbers(1, 2, 3) == (1, 2)
+    assert find_smallest_two_numbers(1, 3, 2) == (1, 2)
+    assert find_smallest_two_numbers(3, 2, 1) == (1, 2)
 
 
 def test_gift_wrapping_paper():
     assert calculate_gift_wrapping_paper(2, 3, 4) == 58
-    assert calculate_gift_wrapping_paper(1,1,10 == 43)
+    assert calculate_gift_wrapping_paper(1, 1, 10 == 43)
+
+
+def test_calculate_ribbon_length_to_order():
+    assert calculate_ribbon_length_to_order(2, 3, 4) == 34
+    assert calculate_ribbon_length_to_order(1, 1, 10) == 14
+    assert calculate_ribbon_length_to_order(2, 5, 8) == 94
