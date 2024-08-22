1075c544897049f6568313bd0918bda7cd232b12
Arin Minasian <arinmsn@gmail.com>
2024-08-22 00:06:12 -0400

    2015 - Day 3: Part II - Added some more tests for Part II

--- Advent of Code/2015/Day 3/test_got_at_least_one_present.py
+++ Advent of Code/2015/Day 3/test_got_at_least_one_present.py
@@ -1,4 +1,5 @@
 from got_at_least_one_present import count_houses_with_presents
+from got_at_least_one_present_part2 import count_houses_with_presents_part2
 
 
 def test_count_houses_with_presents():
@@ -7,3 +8,11 @@ def test_count_houses_with_presents():
     assert count_houses_with_presents("^v^v^v^v^v") == 2
     assert count_houses_with_presents(">>>>>") == 6
     assert count_houses_with_presents("^^vv") == 3
+    assert count_houses_with_presents("") == 1
+
+
+# Robo-Santa helps Santa deliver the presents
+def test_count_houses_with_presents_part2():
+    assert count_houses_with_presents_part2("^v") == 3
+    assert count_houses_with_presents_part2("^>v<") == 3
+    assert count_houses_with_presents_part2("^v^v^v^v^v") == 11
