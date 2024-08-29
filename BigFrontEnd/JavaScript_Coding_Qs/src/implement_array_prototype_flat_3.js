7268dc0d8edbfac1967b9344deb6e69b2d566210
Arin Minasian <arinmsn@gmail.com>
2024-08-28 20:31:09 -0400

    Current iterative approach does not handle Infinity well

--- BigFrontEnd/JavaScript_Coding_Qs/src/implement_array_prototype_flat_3.js
+++ BigFrontEnd/JavaScript_Coding_Qs/src/implement_array_prototype_flat_3.js
@@ -8,7 +8,8 @@ Here is an example to illustrate
 Follow up: Are you able to solve it both recursively and iteratively?
 */
 
-function flat(arr, depth = 1) {
+// The resursive way
+function flatRecursive(arr, depth = 1) {
     return arr.reduce((result, item) => {
         if (Array.isArray(item) && depth > 0) {
             result.push(...flat(item, depth - 1))
@@ -19,12 +20,33 @@ function flat(arr, depth = 1) {
     }, [])
 }
 
+function flatIterative(arr, depth = 1) {
+    let result = arr.slice(0)
+    while (depth-- > 0) {
+        let count = result.lenth
+        while (count-- > 0) {
+            const head = result.shift()
+            if (Array.isArray(head)) {
+                result.push(...head)
+            } else {
+                result.push(head)
+            }
+        }
+    }
+    return result
+}
 
-console.log([1, [2], [3, [4]]].flat());
-console.log([1, [2], [3, [4]]].flat(1));
-console.log([1, [2], [3, [4]]].flat(2));
-console.log([1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]].flat(Infinity));
+/*
+console.log([1, [2], [3, [4]]].flatRecursive());
+console.log([1, [2], [3, [4]]].flatRecursive(1));
+console.log([1, [2], [3, [4]]].flatRecursive(2));
+console.log([1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]].flatRecursive(Infinity));
+*/
 
+console.log(flatIterative([1, [2], [3, [4]]]));
+console.log(flatIterative([1, [2], [3, [4]]], 1));
+console.log(flatIterative([1, [2], [3, [4]]], 2));
+console.log(flatIterative([1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]], Infinity));
 
 
 
