85630aacbaec1b4f459dd7ce2168c3fd0b20938e
Arin Minasian <arinmsn@gmail.com>
2024-08-28 20:39:05 -0400

    Iterative approach still takes long time with Infinity invovled

--- BigFrontEnd/JavaScript_Coding_Qs/src/implement_array_prototype_flat_3.js
+++ BigFrontEnd/JavaScript_Coding_Qs/src/implement_array_prototype_flat_3.js
@@ -8,6 +8,8 @@ Here is an example to illustrate
 Follow up: Are you able to solve it both recursively and iteratively?
 */
 
+const measurePerformance = require("./helpers/measure_performance");
+
 // The resursive way
 function flatRecursive(arr, depth = 1) {
     return arr.reduce((result, item) => {
@@ -22,11 +24,14 @@ function flatRecursive(arr, depth = 1) {
 
 function flatIterative(arr, depth = 1) {
     let result = arr.slice(0)
+    let hasArrayItem = true
     while (depth-- > 0) {
+        hasArrayItem = false
         let count = result.lenth
         while (count-- > 0) {
             const head = result.shift()
             if (Array.isArray(head)) {
+                hasArrayItem = true
                 result.push(...head)
             } else {
                 result.push(head)
@@ -46,7 +51,7 @@ console.log([1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]].flatRecursive(Infinity));
 console.log(flatIterative([1, [2], [3, [4]]]));
 console.log(flatIterative([1, [2], [3, [4]]], 1));
 console.log(flatIterative([1, [2], [3, [4]]], 2));
-console.log(flatIterative([1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]], Infinity));
+console.log(measurePerformance(flatIterative([1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]], Infinity)));
 
 
 
