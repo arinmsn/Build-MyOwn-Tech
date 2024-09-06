df3937b87af4cfdd0ad4c0d518b50baee29daa0e
Arin Minasian <arinmsn@gmail.com>
2024-09-06 00:48:13 -0400

    More tests for the new range(start, end, step = 1)

--- Books/Eloquent_JavaScript/Exercises/Ch_4/The Sum of Range/tests/sumOfRange.test.js
+++ Books/Eloquent_JavaScript/Exercises/Ch_4/The Sum of Range/tests/sumOfRange.test.js
@@ -14,3 +14,22 @@ describe("it should return correct sum given an array", () => {
     assert.deepStrictEqual(sum([0, 1, 2, 3, 4, 5]), 15);
   });
 });
+
+describe("returns array with correct start and end for given step", () => {
+  it("should return a warning and empty string if user enters 0 for step", () => {
+    assert.deepStrictEqual(range(0, 5, 0), []);
+    assert.deepStrictEqual(range(1, 10, 0), []);
+  });
+
+  it("should return an incremented array of numbers given a positive step", () => {
+    assert.deepStrictEqual(range(0, 5, 1), [0, 1, 2, 3, 4, 5]);
+    assert.deepStrictEqual(range(0, 10, 4), [0, 4, 8]);
+    assert.deepStrictEqual(range(0, 10, 3), [0, 3, 6, 9]);
+  });
+
+  it("should return an decremented array of numbers given a negative step", () => {
+    assert.deepStrictEqual(range(6, 0, -1), [6, 5, 4, 3, 2, 1, 0]);
+    assert.deepStrictEqual(range(8, 1, -3), [8, 5, 2]);
+    assert.deepStrictEqual(range(19, 12, -4), [19, 15]);
+  });
+});
