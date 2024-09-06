2be35a26d725c10c88f671807a0512b1880b6991
Arin Minasian <arinmsn@gmail.com>
2024-09-06 00:00:30 -0400

    Tests cover sum() function

--- Books/Eloquent_JavaScript/Exercises/Ch_4/The Sum of Range/tests/sumOfRange.test.js
+++ Books/Eloquent_JavaScript/Exercises/Ch_4/The Sum of Range/tests/sumOfRange.test.js
@@ -1,9 +1,16 @@
 const assert = require("assert");
-const range = require("../sumOfRange");
+const { range, sum } = require("../sumOfRange");
 
 describe("it should return an array with correct start/end numbers", () => {
   it("should return correct numbers in the array", () => {
     assert.deepStrictEqual(range(1, 10), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
     assert.deepStrictEqual(range(0, 5), [0, 1, 2, 3, 4, 5]);
+  });
+});
+
+describe("it should return correct sum given an array", () => {
+  it("should return correct sum", () => {
+    assert.deepStrictEqual(sum(range(1, 10)), 55);
+    assert.deepStrictEqual(sum([0, 1, 2, 3, 4, 5]), 15);
   });
 });
